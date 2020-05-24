bbc21fd71b5b38da7e5df0064622cae6622569c5
Yoo isae <dbdlto@naver.com>
2020-05-24 23:22:14 +0900

    creat D08.c

---
+++ D08.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int a,b,c;
+  int x_begin, x_end;
+  int x,y;
+  printf("2차 함수의 계수 a와 b와 c를 입력");
+  scanf("%d %d %d", &a, &b, &c);
+  printf("x좌표의 시작 값과 끝 값을 입력");
+  scanf("%d %d", &x_begin, &x_end);
+  
+  for(int i = x_begin ; i <= x_end ; i++){
+    x = i;
+    y = a*x*x+b*x+c;
+    printf("좌표 (%d ,%d)", x ,y);
+  }
+  return 0;
+}
